a864fc69092c0bbc107f0c1307fdd70f4b9aa303
Marcos Pinho <marcosjava2008@gmail.com>
2016-08-19 16:12:05 -0300

    Criando o pagina de cadastro de portos.

--- src/main/java/com/sulnorte/frota/controller/PortoController.java
+++ src/main/java/com/sulnorte/frota/controller/PortoController.java
@@ -9,10 +9,17 @@ public class PortoController {
 
     private static final String VIEW_LIST = "porto/List";
     private static final String ACTION_LIST = "/list";
+    private static final String VIEW_FORM = "porto/Form";
+    private static final String ACTION_CREATE = "/create";
 
     @RequestMapping(value = ACTION_LIST)
     public String home(){
         return VIEW_LIST;
+    }
+
+    @RequestMapping(value = ACTION_CREATE)
+    public String prepareCreate(){
+        return VIEW_FORM;
     }
 
 }

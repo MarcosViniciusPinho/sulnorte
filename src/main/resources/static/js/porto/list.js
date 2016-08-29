68b17794135fd801391ec813d97086b74dc1aaa6
Marcos Pinho <marcosjava2008@gmail.com>
2016-08-28 21:18:58 -0300

    Correção de BUG na exclusão de Porto.

--- src/main/resources/static/js/porto/list.js
+++ src/main/resources/static/js/porto/list.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
         var id = button.data('id');
         var nome = button.data('nome');
         var modal = $(this);
-        var action = $("#formExcluir").attr('action');
-        $("#formExcluir").attr('action', action + id);
+        var action = $('#formExcluir').data('url-base');
+        $('#formExcluir').attr('action', action + id);
         modal.find('.modal-body span').html('Tem certeza que deseja excluir o porto <strong>' + nome + '</strong>?');
     })
 });

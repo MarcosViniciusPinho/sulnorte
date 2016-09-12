5799d7e8d550d1a4e3998e11f5f34240f3024048
Marcos Pinho <marcosjava2008@gmail.com>
2016-09-12 20:25:22 -0300

    Corrigindo script de filial

--- src/main/resources/scripts/script4_carga_inicial_na_tabela_filial.sql
+++ src/main/resources/scripts/script4_carga_inicial_na_tabela_filial.sql
@@ -1,9 +1,9 @@
-INSERT INTO pais (id, nome) VALUES(1, 'Rio Grande');
-INSERT INTO pais (id, nome) VALUES(2, 'Paranaguá');
-INSERT INTO pais (id, nome) VALUES(3, 'Santos');
-INSERT INTO pais (id, nome) VALUES(4, 'Sepetiba');
-INSERT INTO pais (id, nome) VALUES(5, 'Rio de Janeiro');
-INSERT INTO pais (id, nome) VALUES(6, 'Vitória');
-INSERT INTO pais (id, nome) VALUES(7, 'Salvador');
-INSERT INTO pais (id, nome) VALUES(8, 'Temadre');
-INSERT INTO pais (id, nome) VALUES(9, 'Maceió');
+INSERT INTO filial (id, nome) VALUES(1, 'Rio Grande');
+INSERT INTO filial (id, nome) VALUES(2, 'Paranaguá');
+INSERT INTO filial (id, nome) VALUES(3, 'Santos');
+INSERT INTO filial (id, nome) VALUES(4, 'Sepetiba');
+INSERT INTO filial (id, nome) VALUES(5, 'Rio de Janeiro');
+INSERT INTO filial (id, nome) VALUES(6, 'Vitória');
+INSERT INTO filial (id, nome) VALUES(7, 'Salvador');
+INSERT INTO filial (id, nome) VALUES(8, 'Temadre');
+INSERT INTO filial (id, nome) VALUES(9, 'Maceió');

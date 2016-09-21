6f78a55a3212bc8942352069da6ca845b175390e
Marcos Pinho <marcosjava2008@gmail.com>
2016-09-21 01:39:57 -0300

    Release 2 :: Implementado o multiselect para seleção de portos em Armador(Já está funcionando).

--- src/main/java/com/sulnorte/frota/controller/ArmadorController.java
+++ src/main/java/com/sulnorte/frota/controller/ArmadorController.java
@@ -1,8 +1,10 @@
 package com.sulnorte.frota.controller;
 
 import com.sulnorte.frota.business.facade.IArmadorFacade;
+import com.sulnorte.frota.business.facade.IPortoFacade;
 import com.sulnorte.frota.dto.ArmadorDTO;
 import com.sulnorte.frota.entity.Armador;
+import com.sulnorte.frota.entity.Porto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,15 @@ public class ArmadorController extends EnderecoController<ArmadorDTO, Armador>{
     private static final String VIEW_FORM = "armador/Form";
     private static final String LISTAR_PAISES = "listarPaises";
     private static final String LISTAR_ARMADORES = "listarArmadores";
+    private static final String LISTAR_PORTOS = "listarPortos";
     private static final String REDIRECT_LIST = "redirect:/armador/list";
     private static final String VIEW_DETAIL = "armador/Detail";
 
     @Autowired
     private IArmadorFacade<Armador> armadorFacade;
+
+    @Autowired
+    private IPortoFacade<Porto> portoFacade;
 
     /**
      * {@inheritDoc}
@@ -39,6 +45,7 @@ public class ArmadorController extends EnderecoController<ArmadorDTO, Armador>{
     protected ModelAndView onPrepareCreate(){
         ModelAndView mv = new ModelAndView(VIEW_FORM);
         mv.addObject(LISTAR_PAISES, enderecoFacade.findAllPais());
+        mv.addObject(LISTAR_PORTOS, this.portoFacade.findAllPorto());
         mv.addObject(new ArmadorDTO());
         return mv;
     }
@@ -50,6 +57,7 @@ public class ArmadorController extends EnderecoController<ArmadorDTO, Armador>{
     protected ModelAndView onPrepareUpdateOrDetail(String view, Long id){
         ModelAndView mv = new ModelAndView(view);
         mv.addObject(LISTAR_PAISES, enderecoFacade.findAllPais());
+        mv.addObject(LISTAR_PORTOS, this.portoFacade.findAllPorto());
         mv.addObject(this.armadorFacade.findByIdArmador(id));
         return mv;
     }
---
+++ src/main/resources/static/js/armador/form.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('#portos').multiSelect();
+});

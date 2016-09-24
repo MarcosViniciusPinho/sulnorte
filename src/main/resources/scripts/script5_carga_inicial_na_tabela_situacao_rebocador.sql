ecdabb2dc81ef9a99c61603757a467d861ee2890
MarcosViniciusPinho <marcosjava2008@gmail.com>
2016-09-24 17:54:25 -0300

    Release 3 :: Criando carga inicial para Rebocador.

---
+++ src/main/resources/scripts/script5_carga_inicial_na_tabela_situacao_rebocador.sql
@@ -0,0 +1,7 @@
+INSERT INTO situacao_rebocador (id, nome) values(1, 'Afretado');
+INSERT INTO situacao_rebocador (id, nome) values(2, 'Apoio Portuário');
+INSERT INTO situacao_rebocador (id, nome) values(3, 'Em construção');
+INSERT INTO situacao_rebocador (id, nome) values(4, 'Docagem');
+INSERT INTO situacao_rebocador (id, nome) values(5, 'Indisponível');
+INSERT INTO situacao_rebocador (id, nome) values(6, 'Manutenção');
+INSERT INTO situacao_rebocador (id, nome) values(7, 'Parado');
---
+++ src/main/resources/scripts/script6_carga_inicial_na_tabela_propulsao.sql
@@ -0,0 +1,5 @@
+INSERT INTO propulsao (id, nome) values(1, 'Asd');
+INSERT INTO propulsao (id, nome) values(2, 'Conv');
+INSERT INTO propulsao (id, nome) values(3, 'Conv(1)');
+INSERT INTO propulsao (id, nome) values(4, 'Conv(2)');
+INSERT INTO propulsao (id, nome) values(5, 'Conv(3)');

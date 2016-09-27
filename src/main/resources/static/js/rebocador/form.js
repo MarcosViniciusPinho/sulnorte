d3f1220c74811204992552e267448f44f03a1d1f
MarcosViniciusPinho <marcosjava2008@gmail.com>
2016-09-26 21:45:24 -0300

    Release 3 :: Adicionando no campo Ano só entrar com valor numérico em Rebocador.

--- src/main/resources/static/js/rebocador/form.js
+++ src/main/resources/static/js/rebocador/form.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     mascaraNumeroDecimal("#bhp");
     mascaraNumeroDecimal("#bp");
     formatarCamposAntesDeGravar();
+    permitirSomenteNumeros($('input[id="ano"]'));
 });
 
 function formatarCamposAntesDeGravar(){
@@ -20,4 +21,20 @@ function mascaraNumeroDecimal(elemento){
 function transformarDecimaisAntesDeGravar(elemento){
     var valor = $(elemento).val().replace('.','').replace(',','.');
     $(elemento).val(valor);
+}
+
+function permitirSomenteNumeros(input){
+    $(input).unbind('keyup').bind('keyup',function(e){
+        var thisVal = $(this).val();
+        var tempVal = "";
+        for(var i = 0; i<thisVal.length; i++){
+            if(RegExp(/^[0-9]$/).test(thisVal.charAt(i))){
+                tempVal += thisVal.charAt(i);
+                if(e.keyCode == 8){
+                    tempVal = thisVal.substr(0,i);
+                }
+            }
+        }
+        $(this).val(tempVal);
+    });
 }

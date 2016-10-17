63dedfd7f56243f8f74312d3d684a41ba7ba49bf
MarcosViniciusPinho <marcosjava2008@gmail.com>
2016-10-17 16:48:36 -0200

    Release 3 :: Finalizado a implementação na tela de Cadastrar/Alterar a busca dinâmica em Rebocador -  Ao selecionar um Armador buscar os Portos associados ao mesmo(Solicitado por Débora).

--- src/main/resources/static/js/cadastro/rebocador/form.js
+++ src/main/resources/static/js/cadastro/rebocador/form.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     mascaraNumeroDecimal("#bhp");
     mascaraNumeroDecimal("#bp");
     permitirSomenteNumeros($('input[id="ano"]'));
+    loadPorto();
 });
 
 function mascaraNumeroDecimal(elemento){
@@ -23,5 +24,72 @@ function permitirSomenteNumeros(input){
             }
         }
         $(this).val(tempVal);
+    });
+}
+
+function loadPorto(){
+    var id = $("#id").val();
+    if(id === ""){
+        carregarPortosAoSalvar();
+    } else{
+        carregarPortosAoAlterar();
+    }
+}
+
+function carregarPortosAoSalvar(){
+    $("#porto").html('<option value="" selected="selected">Selecione um porto</option>');
+    $("#armador option:first").attr('selected','selected');
+    trocarPortoComDeterminadoArmador();
+}
+
+function carregarPortosAoAlterar(){
+    $("#porto").html('<option value="" selected="selected">Selecione um porto</option>');
+    executarListagemPortos();
+    trocarPortoComDeterminadoArmador();
+}
+
+function trocarPortoComDeterminadoArmador(){
+    $("#armador").change(function() {
+        $("#porto").html("");
+        $("#porto").html('<option value="" selected="selected">Selecione um porto</option>');
+        executarListagemPortos();
+    });
+}
+
+function executarListagemPortos(){
+    var armador = $("#armador").val();
+    if(armador !== ""){
+        executarJson($("#path").val() + $("#pathController").val() + '/listarPortoPorArmador', armador);
+    }
+}
+
+function selecionarPortoAoEditar(){
+    var id = $("#id").val();
+    if(id !== ""){
+        var idPorto = $("#portoId").val();
+        $("#porto").val(idPorto);
+        var result = $("#porto").val();
+        if(result === null){
+            $("#porto").val("");
+        }
+    }
+}
+
+function executarJson(urlMetodo, parametro) {
+    $.ajax({
+        url : urlMetodo,
+        type: 'POST',
+        dataType: 'json',
+        data: parametro,
+        contentType: 'application/json',
+        mimeType: 'application/json',
+        success : function(data) {
+            var transform = {'tag':'option value=\"\${id}\"','html':'\${nome}'};
+            $("#porto").append(json2html.transform(data,transform));
+            selecionarPortoAoEditar();
+        },
+        error:function(data,status,er) {
+            console.log('Erro ao enviar<br>' + data +'<br>'+status+'<br>'+er);
+        }
     });
 }

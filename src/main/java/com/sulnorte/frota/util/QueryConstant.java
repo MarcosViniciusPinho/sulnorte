c013b2c2fb555f55c32044a48c37db8cb7074587
MarcosViniciusPinho <marcosjava2008@gmail.com>
2016-10-19 21:50:46 -0200

    Release 3 :: Correção do indicador 'Minor' do Sonar: Move this variable to comply with Java Code Conventions

--- src/main/java/com/sulnorte/frota/util/QueryConstant.java
+++ src/main/java/com/sulnorte/frota/util/QueryConstant.java
@@ -5,8 +5,6 @@ package com.sulnorte.frota.util;
  */
 public class QueryConstant {
 
-    private QueryConstant() {}
-
     public static final String FIND_BY_ID_PORTO_ON_ARMADOR_PORTO = "select ap.id_porto from armador_porto ap " +
             "inner join porto p on ap.id_porto=p.id " +
             "where ap.id_porto = ?1";
@@ -14,5 +12,7 @@ public class QueryConstant {
     public static final String FIND_ALL_IDS_PORTO_ON_ARMADOR_PORTO = "select ap.id_porto from armador_porto ap " +
             "inner join porto p on ap.id_porto=p.id " +
             "where ap.id_armador = ?1";
+
+    private QueryConstant() {}
 
 }
